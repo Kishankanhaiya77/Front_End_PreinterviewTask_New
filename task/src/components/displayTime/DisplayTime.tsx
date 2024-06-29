fad8511084832bf44f6a09c19c32b64196bbe613
Kishan K <kisnison@gmail.com>
2024-06-29 19:06:57 +0530

    added condition in displayTime component

--- task/src/components/displayTime/DisplayTime.tsx
+++ task/src/components/displayTime/DisplayTime.tsx
@@ -8,10 +8,15 @@ interface props {
 const DisplayTime: React.FC<props> = ({ lastClicked }) => {
     return (
         <>
-            <div className=" TimeItem flex text-center p-6 mr-3 bg-slate-300 font-serif shadow-xl">
-                Local time:{" "}
-                {lastClicked !== undefined ? lastClicked.toString() : "Never"}
-            </div>
+            {!lastClicked ? <div className='TimeItem'> 
+                Please click on the button to get local time 
+                </div>
+                 :
+                <div className=" TimeItem flex text-center p-6 mr-3 bg-slate-300 font-serif shadow-xl">
+                    Local time:{" "}
+                    {lastClicked !== undefined ? lastClicked.toString() : "Never"}
+                </div>
+            }
         </>
     );
 }
